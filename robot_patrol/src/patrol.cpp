5256f2a8b10393b1ee549ff8340f92a34e747d90
Mashiat <mashiatraidahnaira@gmail.com>
2025-01-11 16:32:03 +0000

    3. Get the laser data

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,4 +1,32 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 
-class Patrol : public rclcpp::Node {};
+class Patrol : public rclcpp::Node {
+public:
+  Patrol() : Node("patrol_node") {
+    subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/scan", 10,
+        std::bind(&Patrol::laser_callback, this, std::placeholders::_1));
+  }
+
+private:
+  void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    int size = static_cast<int>(msg->ranges.size());
+    RCLCPP_INFO(this->get_logger(), "Laser array size is: %d", size);
+    RCLCPP_INFO(this->get_logger(), "Laser Scan Value[Front]: %.2f",
+                msg->ranges[330]);
+    RCLCPP_INFO(this->get_logger(), "Laser Scan Value[Right]: %.2f",
+                msg->ranges[0]);
+    RCLCPP_INFO(this->get_logger(), "Laser Scan Value[Left]: %.2f",
+                msg->ranges[659]);
+  }
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+};
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  std::shared_ptr<Patrol> node = std::make_shared<Patrol>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
