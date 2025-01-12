74cd24d0572a954c1b08693204a15adffd98a9b0
Mashiat <mashiatraidahnaira@gmail.com>
2025-01-12 05:19:10 +0000

    7. Fixed angular speed for the robot at corners and nearby an obstacle

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -38,6 +38,14 @@ private:
     float largest_distance_angle = 0.0;
     float closest_distance = std::numeric_limits<float>::max();
     float closest_distance_angle = 0.0;
+    size_t closest_index;
+
+    /*# Calculate the index for the specific angle (0 degrees in this case)
+    specific_angle = 0.0  # Change this value if you're looking for a different
+    angle*/
+    // index = int((specific_angle - angle_min) / angle_increment)
+    // So, -15 degrees, index = (-15-(-180)) / 0.5 = 330 [All radian values
+    // converted to degrees], & 15 degrees, index = (15-(-180)) / 0.5 = 390
 
     for (size_t i = 0; i < msg->ranges.size(); i++) {
       float angle = msg->angle_min + i * msg->angle_increment;
@@ -49,33 +57,63 @@ private:
         } else if (!std::isinf(dist) && dist < closest_distance) {
           closest_distance = dist;
           closest_distance_angle = angle;
+          if (i >= 330 && i <= 390) {
+            closest_index = i;
+          }
         }
       }
     }
 
-    if (closest_distance <= 0.35) {
-      direction_ = largest_distance_angle;
-      if (closest_distance_angle >= 0.0) {
-        direction_ -= 2.5;
-      } else {
-        direction_ += 2.5;
-      }
-    } else {
-      direction_ = 0.0;
-    }
+    direction_ = largest_distance_angle;
+    twist_msg = this->angularSpeed(closest_distance, direction_,
+                                   closest_distance_angle, closest_index);
 
     RCLCPP_INFO(this->get_logger(), "The Closest distance is %.2f",
                 closest_distance);
+    RCLCPP_INFO(this->get_logger(), "The closest distance angle is %.2f",
+                closest_distance_angle);
+    RCLCPP_INFO(this->get_logger(), "The closest index is %.2ld",
+                closest_index);
     RCLCPP_INFO(this->get_logger(), "The Safest direction is %.2f", direction_);
   }
 
+  geometry_msgs::msg::Twist angularSpeed(float closest_distance,
+                                         float direction_,
+                                         float closest_distance_angle,
+                                         size_t closest_index) {
+
+    auto msg = geometry_msgs::msg::Twist();
+
+    if (closest_distance <= 0.35) {
+      if ((closest_distance_angle >= 0.0) ||
+          ((closest_index > 360) && (closest_index <= 390))) {
+        msg.linear.x = 0.1;
+        direction_ = -1.4;
+        msg.angular.z = direction_ / 2;
+        if (closest_distance < 0.30) {
+          msg.linear.x = 0.0;
+          msg.angular.z = direction_ / 2;
+        }
+      } else if ((closest_distance_angle < 0.0) ||
+                 ((closest_index >= 330) && (closest_index < 360))) {
+        msg.linear.x = 0.1;
+        direction_ = 1.4;
+        msg.angular.z = direction_ / 2;
+        if (closest_distance < 0.30) {
+          msg.linear.x = 0.0;
+          msg.angular.z = direction_ / 2;
+        }
+      }
+    } else {
+      msg.linear.x = 0.1;
+      direction_ = 0.0;
+      msg.linear.z = direction_ / 2;
+    }
+
+    return msg;
+  }
+
   void timer_callback() {
-    geometry_msgs::msg::Twist twist_msg;
-
-    twist_msg.linear.x = 0.1;
-
-    twist_msg.angular.z = direction_ / 2;
-
     publisher_->publish(twist_msg);
 
     RCLCPP_INFO(this->get_logger(), "Linear velocity is %f",
@@ -88,6 +126,7 @@ private:
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   rclcpp::CallbackGroup::SharedPtr timer_callback_group;
   rclcpp::TimerBase::SharedPtr timer_;
+  geometry_msgs::msg::Twist twist_msg;
   float direction_;
 };
 
