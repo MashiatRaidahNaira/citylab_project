6a7520d00e91534d80b90468cb634ae8dbf1e379
Mashiat <mashiatraidahnaira@gmail.com>
2025-01-15 09:16:47 +0000

    3. Create a custom interface

--- robot_patrol/src/direction_service.cpp
+++ robot_patrol/src/direction_service.cpp
@@ -1,28 +1,29 @@
+#include "custom_interfaces/srv/get_direction.hpp"
 #include "rclcpp/executors.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/service.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
-#include "std_srvs/srv/empty.hpp"
 #include <functional>
 #include <memory>
 
-using Empty = std_srvs::srv::Empty;
+using GetDirection = custom_interfaces::srv::GetDirection;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
 class DirectionService : public rclcpp::Node {
 public:
   DirectionService() : Node("direction_service") {
-    srv_ = create_service<Empty>(
+    srv_ = create_service<GetDirection>(
         "direction_service",
         std::bind(&DirectionService::direction_callback, this, _1, _2));
   }
 
 private:
-  rclcpp::Service<Empty>::SharedPtr srv_;
+  rclcpp::Service<GetDirection>::SharedPtr srv_;
 
-  void direction_callback(const std::shared_ptr<Empty::Request> request,
-                          const std::shared_ptr<Empty::Response> response) {}
+  void
+  direction_callback(const std::shared_ptr<GetDirection::Request> request,
+                     const std::shared_ptr<GetDirection::Response> response) {}
 };
 
 int main(int argc, char *argv[]) {
